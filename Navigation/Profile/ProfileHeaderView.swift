1c2064a56ef3975b04be91fca3863fa197edbfd0
Pavel Savvateev <savvateev@icloud.com>
2026-04-29 15:55:44 +0800

    backgroundColor is lightGray

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Pavel Savvateev on 29.04.2026.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
