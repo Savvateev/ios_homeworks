cd99edb80c950433856b8253e7db3d1ccd6e973a
Pavel Savvateev <savvateev@icloud.com>
2026-05-13 12:51:19 +0800

    The task is fixed

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,12 +9,124 @@ import UIKit
 
 class ProfileHeaderView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    // Фон
+    private let headerBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
 
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        //imageView.image = UIImage(systemName: "person.crop.circle.fill")
+        imageView.image = UIImage(named: "Cat")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 60
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+
+    private let fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Кошка"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Спит"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let setStatusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Статус", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 4
+        
+        // Тень
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        
+        // сабвью
+        addSubview(headerBackgroundView)
+        addSubview(avatarImageView)
+        addSubview(fullNameLabel)
+        addSubview(statusLabel)
+        addSubview(setStatusButton)
+        
+        // Обработка
+        setStatusButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) error")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // 1. Получаем отступ safeArea
+        let topPadding = self.safeAreaInsets.top
+        let screenWidth = self.bounds.width
+        
+        // 2. Аватарка
+        avatarImageView.frame = CGRect(x: 16, y: topPadding + 16, width: 120, height: 120)
+        
+        // 3. Имя
+        fullNameLabel.sizeToFit() // Рассчитываем размер текста
+        fullNameLabel.frame = CGRect(
+            x: (screenWidth - fullNameLabel.frame.width) / 2,
+            y: topPadding + 27,
+            width: fullNameLabel.frame.width,
+            height: fullNameLabel.frame.height
+        )
+        
+        // 4. Кнопка
+        let buttonWidth = screenWidth - 32
+        setStatusButton.frame = CGRect(
+            x: 16,
+            y: avatarImageView.frame.maxY + 16,
+            width: buttonWidth,
+            height: 50
+        )
+        
+        // 5. Статус
+        statusLabel.sizeToFit()
+        statusLabel.frame = CGRect(
+            x: (screenWidth - statusLabel.frame.width) / 2,
+            y: setStatusButton.frame.minY - 34 - statusLabel.frame.height,
+            width: statusLabel.frame.width,
+            height: statusLabel.frame.height
+        )
+        
+        // 6. Серый фон
+        headerBackgroundView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: screenWidth,
+            height: setStatusButton.frame.maxY + 16
+        )
+    }
+
+    @objc func buttonPressed() {
+        print("Текущий статус: \(statusLabel.text ?? "")")
+    }
 }
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,21 +8,18 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    private let profileHeaderView = ProfileHeaderView()
+    
     override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .lightGray
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        title = "Профиль"
+        view.addSubview(profileHeaderView)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHeaderView.frame = view.frame
     }
-    */
-
 }
