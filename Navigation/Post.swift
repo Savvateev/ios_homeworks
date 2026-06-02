793cf1236a11c50634428d90fc3db038ba3afa22
Pavel Savvateev <savvateev@icloud.com>
2026-06-02 08:17:29 +0800

    posts data added

--- Navigation/Post.swift
+++ Navigation/Post.swift
@@ -7,3 +7,11 @@ struct Post {
     var likes: Int
     var views: Int
 }
+
+var posts: [Post] = [
+    Post(author: "LeoTolstoy", description: "пишу новый роман", image: "leotolstoy", likes: 10, views: 100 ),
+    Post(author: "Medinsky", description: "переписываю историю", image: "medinsky", likes: 0, views: 1000 ),
+    Post(author: "Selhoznadzor", description: "запрещаю армянскую форель", image: "rshn", likes: 5, views: 120 ),
+    Post(author: "Roskomnadzor", description: "блокирую интернет", image: "rkn", likes: 1, views: 10000 )
+]
+
