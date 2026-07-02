d501e176caa82ad8ffb739c0aeb4b5adcb97fd10
Pavel Savvateev <savvateev@icloud.com>
2026-07-02 11:59:44 +0800

    filters added

--- Navigation/Profile/PostTableViewCell.swift
+++ Navigation/Profile/PostTableViewCell.swift
@@ -1,23 +1,17 @@
-//
-//  PostTableViewCell.swift
-//  Navigation
-//
-//  Created by Pavel Savvateev on 05.06.2026.
-//
-
 import UIKit
 import StorageService
+import SnapKit
+import iOSIntPackage
 
 class PostTableViewCell: UITableViewCell {
     
-    // UI элементы
+    // MARK: - UI Elements
     
     private let authorLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16, weight: .bold)
         label.textColor = .black
         label.numberOfLines = 1
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -25,7 +19,6 @@ class PostTableViewCell: UITableViewCell {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .black
-        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
@@ -34,24 +27,23 @@ class PostTableViewCell: UITableViewCell {
         label.font = .systemFont(ofSize: 14)
         label.textColor = .systemGray
         label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let likesLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16)
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let viewsLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16)
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
+    // MARK: - Init
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupLayout()
@@ -61,13 +53,27 @@ class PostTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
-    // Метод для заполнения данными
+    // MARK: - Public Methods
+    
     func configure(with post: Post) {
         authorLabel.text = post.author
         descriptionLabel.text = post.description
-        postImageView.image = UIImage(named: post.image)
         likesLabel.text = "Likes: \(post.likes)"
         viewsLabel.text = "Views: \(post.views)"
+        
+        // Загружаем оригинальное фото и применяем фильтр
+        if let image = UIImage(named: post.image) {
+            setupImageWithFilter(image)
+        }
+    }
+    
+    private func setupImageWithFilter(_ sourceImage: UIImage) {
+        let processor = ImageProcessor()
+        
+        //  .chrome, .colorInvert, .noir, .fade, .tonal......
+        processor.processImage(sourceImage: sourceImage, filter: .tonal) { [weak self] processedImage in
+            self?.postImageView.image = processedImage
+        }
     }
 
     private func setupLayout() {
@@ -76,35 +82,37 @@ class PostTableViewCell: UITableViewCell {
     }
     
     private func setupHierarchy() {
-        contentView.addSubview(authorLabel)
-        contentView.addSubview(postImageView)
-        contentView.addSubview(descriptionLabel)
-        contentView.addSubview(likesLabel)
-        contentView.addSubview(viewsLabel)
+        [authorLabel, postImageView, descriptionLabel, likesLabel, viewsLabel].forEach {
+            contentView.addSubview($0)
+        }
     }
     
     private func setupConstraints() {
-        NSLayoutConstraint.activate([
-            authorLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
-            authorLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            authorLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
-            postImageView.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 12),
-            postImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            postImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            postImageView.heightAnchor.constraint(equalTo: contentView.widthAnchor), // Квадратное фото
-            
-            descriptionLabel.topAnchor.constraint(equalTo: postImageView.bottomAnchor, constant: 16),
-            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            
-            likesLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
-            likesLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            likesLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
-            
-            viewsLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
-            viewsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            viewsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
-        ])
+        authorLabel.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        postImageView.snp.makeConstraints { make in
+            make.top.equalTo(authorLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(contentView.snp.width)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(postImageView.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        likesLabel.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.bottom.equalToSuperview().offset(-16)
+        }
+        
+        viewsLabel.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview().offset(-16)
+        }
     }
 }
